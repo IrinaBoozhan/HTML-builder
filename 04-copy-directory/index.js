8bb257c943ad16a5b00cb71a49dd9be9b1d877e2
Irina Boozhan <bokhanko-irina@yandex.ru>
2021-11-07 13:51:38 +0300

    04-copy-directory done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const fs = require('fs');
+const path = require('path');
 
+const copyFolderPath = path.join(__dirname, 'files-copy');
+const currentFolderPath = path.join(__dirname, 'files');
+
+function createDirectory(){
+  fs.mkdir(copyFolderPath, { recursive: true }, function(err){ 
+    if (err) throw err; 
+  });
+  fs.readdir(copyFolderPath, function(err, items) {
+    if (err) throw err;  
+    for (let i=0; i<items.length; i++) {
+      let copyPath = path.join(copyFolderPath, `${items[i]}`);
+      fs.unlink(copyPath, err => {
+        if(err) throw err; 
+      });
+    }
+  });}
+createDirectory();
+
+function copyDir(){
+  fs.readdir(currentFolderPath, function(err, items) {
+    if (err) throw err;  
+    for (let i=0; i<items.length; i++) {
+      let currentPath = path.join(currentFolderPath, `${items[i]}`);
+      let copyPath = path.join(copyFolderPath, `${items[i]}`);
+      fs.copyFile(currentPath, copyPath, (err) => {
+        if (err) throw err;
+      });
+    }
+  }
+  );
+}
+copyDir();
